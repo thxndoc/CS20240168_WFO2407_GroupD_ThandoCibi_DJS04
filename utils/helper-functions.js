a389dc0ee70fcea7d54f1f7a36956cf37cd72389
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-03 02:20:15 +0200

    Create custom element and set attributes

--- utils/helper-functions.js
+++ utils/helper-functions.js
@@ -1,21 +1,12 @@
 import { books, authors, genres, BOOKS_PER_PAGE } from '../data.js'
 
 export const createBookPreviewElement = (book) => {
-    const element = document.createElement('button');
-    element.classList = 'preview';
-    element.setAttribute('data-preview', book.id);
-    element.innerHTML = `
-        <img
-            class="preview__image"
-            src="${book.image}"
-        />
-        
-        <div class="preview__info">
-            <h3 class="preview__title">${book.title}</h3>
-            <div class="preview__author">${authors[book.author]}</div>
-        </div>
-    `;
-    return element;
+  const bookElement = document.createElement('book-preview');
+  bookElement.setAttribute('data-preview', book.id);
+  bookElement.setAttribute('image', book.image);
+  bookElement.setAttribute('title', book.title);
+  bookElement.setAttribute('author', authors[book.author]);
+  return bookElement;
 }
 
 export const createGenreOptionElement = (genreId, genreName) => {
